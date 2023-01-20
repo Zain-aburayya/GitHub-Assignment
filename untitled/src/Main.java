562f08833b953f3e4dc1b2358c83a168ef85ed9c
Zain Aburayya <zain.burayya@gmail.com>
2023-01-20 22:53:22 +0300

    Admin add new code

---
+++ untitled/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // question 1# : find if the number is prime or not
+        // question 2# : find if the number is binary or not
+    }
+}
