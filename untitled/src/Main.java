a5d92f1a1a14578e625933d6de91c0edeb52ac04
Zain Aburayya <zain.burayya@gmail.com>
2023-01-20 23:15:46 +0300

    Zain add welcome statement

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // question 1# : find if the number is prime or not
+        // hi I am Zain
+
         // question 2# : find if the number is binary or not
     }
 }
